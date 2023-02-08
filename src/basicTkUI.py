8bf4f40436d2ec0332ba4416f04c2b535010ee57
Humair Ahmed <hue@humairahmed.com>
2023-02-08 07:23:43 -0800

    Update basicTkUI.py

--- src/basicTkUI.py
+++ src/basicTkUI.py
@@ -1,3 +1,28 @@
+/****************************************************************
+
+Program: BasicTkUI Version: 1.0 Date: 02/08/2023 Website: http://www.HumairAhmed.com
+
+Lead Developer: Humair Ahmed
+
+Notes: Basic UI Shell using python Tk library
+
+License:
+
+Open source software being distributed under GPL license. For more information see here: http://www.gnu.org/copyleft/gpl.html.
+
+Can edit and redistribute code as long as above reference of authorship is kept within the code. 
+
+Program Description:
+
+This project contains a basic UI shell using python Tk library. A mini UI with four buttons is displayed. Clicking a botton will execute a function tied to that button as well as show a message on the screen that the action has been executed. This basic foundational UI code using Tk can be easily expanded to define more complicated functions related to the buttons.
+
+Dependencies:
+
+Python 3.11.2 was used. Requires Tkinter/Tk python library.
+****************************************************************/
+
+
+
 from tkinter import *
 from tkinter import messagebox
 
